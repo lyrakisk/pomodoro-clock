ffa61d01d5ff8f9abdd6eb47a5fc103548810798
lyrakisk <24938740+lyrakisk@users.noreply.github.com>
2020-09-09 10:00:38 +0200

    Fixes #9. The start button now only works if there is time left in the timer.

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var timer;
 var length = 25; //starting pomodoro length
 var max_length = 180;
 var state = "stopped";
+var seconds = 0;
+var minutes = length;
 
 function counter(){
-  var seconds = $(".seconds").html();
-  var minutes = $(".minutes").html();
+  seconds = $(".seconds").html();
+  minutes = $(".minutes").html();
 
   if (seconds == 0){
     if(minutes > 0){
@@ -14,16 +16,16 @@ function counter(){
       $(".seconds").html(seconds);
       $(".minutes").html(minutes);
     }
-  }  else{
-     seconds -= 1;
-     if(seconds >= 10){
-       $(".seconds").html(seconds);
-    }  else{
-         $(".seconds").html("0"+seconds);
+  } else{
+      seconds -= 1;
+      if(seconds >= 10){
+        $(".seconds").html(seconds);
+    } else{
+        $(".seconds").html("0"+seconds);
     }
   }
 
-  if(seconds == 0 && minutes == 0){
+  if (seconds == 0 && minutes == 0){
     clearInterval(timer);
     state = "stopped";
     new Audio('clapping.mp3').play()
@@ -34,6 +36,8 @@ function reset(){
   clearInterval(timer);
   $(".minutes").html(length);
   $(".seconds").html("00");
+  seconds = $(".seconds").html();
+  minutes = $(".minutes").html();
 }
 
 function add(){
@@ -54,7 +58,7 @@ function subtract(){
 
 //EVENT HANDLING 
 $(".start").click(function(){
-  if (state == "stopped" || state == "paused"){
+  if ((state == "stopped" || state == "paused") && (minutes > 0 || seconds > 0)){
     timer = setInterval(counter, 1000);
     state = "running";
   }
